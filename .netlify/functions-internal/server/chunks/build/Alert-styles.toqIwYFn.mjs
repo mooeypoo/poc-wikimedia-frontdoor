c5de61a37a110633c96b49bd0f384ff08862d12a
mooeypoo <moriel@gmail.com>
2026-05-27 16:18:25 -0400

    Ignore Netlify build artifacts and untrack .netlify output

--- .netlify/functions-internal/server/chunks/build/Alert-styles.toqIwYFn.mjs
+++
@@ -1,8 +0,0 @@
-const Alert_vue_vue_type_style_index_0_scoped_46dbbe98_lang = ".alert[data-v-46dbbe98]{display:flex;align-items:center;padding:1rem;border:2px solid;border-radius:.5rem;background-color:#fff;box-shadow:0 4px 6px #0000001a;transition:transform .2s ease,box-shadow .2s ease}.alert[data-v-46dbbe98]:hover{transform:translateY(-2px);box-shadow:0 6px 8px #00000026}";
-
-const AlertStyles_toqIwYFn = [
-  Alert_vue_vue_type_style_index_0_scoped_46dbbe98_lang
-];
-
-export { AlertStyles_toqIwYFn as default };
-//# sourceMappingURL=Alert-styles.toqIwYFn.mjs.map
